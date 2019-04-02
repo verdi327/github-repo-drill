b7714ebfd27204cc72e7977ce38ae5c7e4bd1712
Michael Verdi <michael.v.verdi@gmail.com>
2019-04-02 15:34:33 -0400

    added event watcher basci

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 'use strict';
 
+// lookup the api
+// setup the event handler
+
+function watchForm(){
+  $('form#github-search').on('submit', function(event) {
+    event.preventDefault();
+    const username = $('#username').val();
+    event.target.reset();
+    console.log(username);
+  });
+}
+
+function main() {
+  watchForm();
+}
+
+$(main);
